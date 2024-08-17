00dba28dd2db759646086cf1ed6d9ccac0edf28f
takahiro <eoca.sakuma@gmail.com>
2024-08-17 12:11:09 +0900

    fix: userscontrollertest

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "application#hello"
-  get "/signup",  to: "users#new"
+  get "/signup",      to: "users#new"
   get "/users/edit",  to: "users#edit"
-  resources :users
+  get "/users/show",  to: "users#show"
 end
