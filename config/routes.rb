0d369af5351eaf15f8c145e6810ee4b2ea77470a
takahiro <eoca.sakuma@gmail.com>
2024-08-17 16:38:10 +0900

    feat: form_withを作成　custom.scssを作成　scssに追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root "application#hello"
   get "/signup",      to: "users#new"
-  get "/users/edit",  to: "users#edit"
-  get "/users/show",  to: "users#show"
+  resources :users
 end
