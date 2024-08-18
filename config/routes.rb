d1c57a1cfc9cdc557c91fc6425cdab155f7c65bf
takahiro <eoca.sakuma@gmail.com>
2024-08-18 14:27:24 +0900

    feat: ホーム画面用にstaticcontrollerを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root "application#hello"
+  root 'static_pages#home'
+  
+  get 'static_pages/home'
   get  "/signup",    to: "users#new"
   get  "/login",     to: "sessions#new"
   post "/login",     to: "sessions#create"
