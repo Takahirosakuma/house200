208d280224e3be00bc8c22f5a938a5d6e36c3e5e
takahiro <eoca.sakuma@gmail.com>
2024-11-02 09:56:16 +0900

    feat: 投稿を何分前という日本語表示に変更

--- config/application.rb
+++ config/application.rb
@@ -6,5 +6,6 @@ module House200
   class Application < Rails::Application
     config.load_defaults 7.0
     config.active_storage.variant_processor = :mini_magick
+    config.i18n.default_locale = :ja
   end
 end
