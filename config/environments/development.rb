65b7258a444012b77d8db70709c935de7099ac71
takahiro <eoca.sakuma@gmail.com>
2024-11-10 17:35:51 +0900

    fix: ドメイン接続するためconfigにshisukanaie.jp独自ドメインを追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,9 @@ Rails.application.configure do
 
   # Enable server timing
   config.server_timing = true
+  
+  # shizukanaie.jpの独自ドメインを許可
+  config.hosts << "shizukanaie.jp"
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
